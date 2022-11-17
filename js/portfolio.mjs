f8b8d4d79cc7b791b7bbcf502f6d821dde52d050
Tushar Verma <tusharverma@Tushars-MacBook-Pro.local>
2022-11-17 18:07:21 +0530

    feat: improved error handling for missing exchange rates

--- js/portfolio.mjs
+++ js/portfolio.mjs
@@ -8,19 +8,32 @@ export class Portfolio {
         this.moneys = this.moneys.concat(moneys);
     }
     evaluate(currency){
-        let total = this.moneys.reduce((sum, money) => {
-            return sum + this.convert(money, currency);
+        let failures = [];
+        let total = this.moneys.reduce( (sum, money) => {
+            let convertedAmount = this.convert(money, currency);
+            if (convertedAmount === undefined) {
+                failures.push(money.currency + "->" + currency);
+                return sum;
+            }
+            return sum + convertedAmount;
         }, 0);
-        return new Money(total, currency);
+        if (!failures.length) {
+            return new Money(total, currency);
+        }
+        throw new Error("Missing exchange rate(s):[" + failures.join() + "]");
     }
     convert(money, currency){
-        let exchangeRates = new Map(); 1
+        let exchangeRates = new Map();
         exchangeRates.set("EUR->USD", 1.2);
         exchangeRates.set("USD->KRW", 1100);
         if (money.currency === currency) {
             return money.amount;
         }
         let key = money.currency + "->" + currency;
-        return money.amount * exchangeRates.get(key);
+        let rate = exchangeRates.get(key);
+        if (rate === undefined) {
+            return undefined; 
+        }
+        return money.amount * rate;
     }
 }
--- js/test_money.js
+++ js/test_money.js
@@ -35,8 +35,17 @@ class MoneyTest{
         let elevenHundredWon = new Money(1100, "KRW");
         let portfolio = new Portfolio();
         portfolio.add(oneDollar, elevenHundredWon);
-        let expectedValue = new Money(2200, "KRW"); 1
+        let expectedValue = new Money(2200, "KRW");
         assert.deepStrictEqual(portfolio.evaluate("KRW"), expectedValue);
+    }
+    testAdditionWithMultipleMissingExchangeRates() {
+        let oneDollar = new Money(1, "USD");
+        let oneEuro = new Money(1, "EUR");
+        let oneWon = new Money(1, "KRW");
+        let portfolio = new Portfolio();
+        portfolio.add(oneDollar, oneEuro, oneWon);
+        let expectedError = new Error("Missing exchange rate(s):[USD->Kalganid,EUR->Kalganid,KRW->Kalganid]");
+        assert.throws(function() {portfolio.evaluate("Kalganid")}, expectedError);
     }
     runAllTests() {
         let testMethods = this.getAllTestMethods();
